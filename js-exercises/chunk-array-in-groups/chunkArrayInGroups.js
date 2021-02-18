096a9668ff2814828c39aa965d20a3032700d1ac
anu3dev <Email>
2021-02-19 04:07:34 +0530

    day 06 exercises

--- js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,14 +1,15 @@
 function chunkArrayInGroups(array, chunkSize) {
-  if (!Array.isArray(array)){
-    return "Seems passed array is invalid array."
-}
-if (typeof chunkSize !== "number"){
-    return "Seems passed chunkSize is invalid function."
-}
-  if (chunkSize === 0){
+  if (!Array.isArray(array)) {
+    return 'Seems passed array is invalid array.';
+  }
+  if (typeof chunkSize !== 'number') {
+    return 'Seems passed chunkSize is invalid function.';
+  }
+  if (chunkSize === 0) {
     return array;
   }
-  let matrix = Array(Math.ceil(array.length / chunkSize)).fill().map(() => array.splice(0, chunkSize));
+  const matrix = Array(Math.ceil(array.length / chunkSize))
+    .fill().map(() => array.splice(0, chunkSize));
   return matrix;
 }
 
