e618c0ef0e64063ec77fd599b990f362ac04bfb3
anu3dev <Email>
2021-02-18 22:12:42 +0530

    Abbreviate String Code Update

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,13 +1,9 @@
 import { isString } from "util";
 
 function abbreviateString(str) {
-
-	let split_names = str.trim().split(" ");
-    let total_word = split_names.length;
-    if (split_names.length > 1) {
-        return (split_names[0] + " " + split_names[(total_word -1)].charAt(0) + ".");
-    }
-    return split_names;
+	let cleanedString = str.trim().replace(/\s\s+/g, " ").split(" ");
+    let printString = `${cleanedString[0]} ${cleanedString[cleanedString.length - 1].charAt(0)}.`
+    return printString;
 }
 
 export { abbreviateString };
