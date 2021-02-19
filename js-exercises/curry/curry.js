489d8729f87dd9186479e37761bc4e8c20cd3595
anu3dev <Email>
2021-02-19 22:38:26 +0530

    curry solution update

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,12 +1,12 @@
-function curry(args){
-	return function curriedSolution(...value1){
-		if (value1.length >= args.length){
-			return args.apply(this, value1);
-		}
-		return function (...value2){
-			return curriedSolution.apply(this, value1.concat(value2));
-		};
-	};
+function curry(fn) {
+  return function curriedSolution(...value1) {
+    if (value1.length >= fn.length) {
+      return fn.apply(this, value1);
+    }
+    return function curried(...value2) {
+      return curriedSolution.apply(this, value1.concat(value2));
+    };
+  };
 }
 
 export {
