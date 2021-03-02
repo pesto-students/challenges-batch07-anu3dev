0025b70ed635621523678584b88707845c8fc84b
anu3dev <Email>
2021-03-02 16:30:48 +0530

    limit function call count

--- js-exercises/limit-function-call-count/limitFunctionCallCount.js
+++ js-exercises/limit-function-call-count/limitFunctionCallCount.js
@@ -1,5 +1,11 @@
-const limitFunctionCallCount = () => {
-
+const limitFunctionCallCount = (callback, noOfTimes) => {
+  let counter = 0;
+  return (...argsforCallback) => {
+    if (counter < noOfTimes) {
+      counter += 1;
+      return callback(...argsforCallback);
+    } return null;
+  };
 };
 
 export {
