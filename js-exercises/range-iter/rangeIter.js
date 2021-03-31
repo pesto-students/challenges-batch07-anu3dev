4894fc663b34c92a383c8ae6c8078b7f0a93fedd
anu3dev <Email>
2021-03-31 15:35:02 +0530

    test case pass code add

--- js-exercises/range-iter/rangeIter.js
+++ js-exercises/range-iter/rangeIter.js
@@ -1,38 +1,38 @@
-// implemented with * [ Symbol.iterator]()
+// implemented with function* and yield i.e. generator
 
 function rangeIter(lb, ub) {
   if (ub === undefined || typeof ub !== 'number' || lb === undefined || typeof lb !== 'number') {
     throw new TypeError('undefined is not a number');
   }
   const rangeArray = [];
-  const range = {
-    from: lb,
-    to: ub,
-    * [Symbol.iterator]() {
-      for (let value = this.from; value <= this.to; value += 1) {
-        yield value;
-      }
-    },
-  };
-  for (const value of range) {
+  function* generateSequence(start, end) {
+    for (let i = start; i <= end; i += 1) {
+      yield i;
+    }
+  }
+  for (const value of generateSequence(lb, ub)) {
     rangeArray.push(value);
   }
   return rangeArray;
 }
 
-// implemented with function* and yield
+// implemented with * [ Symbol.iterator]() i.e. iterable
 
 // function rangeIter(lb, ub) {
 //   if (ub === undefined || typeof ub !== 'number' || lb === undefined || typeof lb !== 'number') {
 //     throw new TypeError('undefined is not a number');
 //   }
 //   const rangeArray = [];
-//   function* generateSequence(start, end) {
-//     for (let i = start; i <= end; i += 1) {
-//       yield i;
-//     }
-//   }
-//   for (const value of generateSequence(lb, ub)) {
+//   const range = {
+//     from: lb,
+//     to: ub,
+//     * [Symbol.iterator]() {
+//       for (let value = this.from; value <= this.to; value += 1) {
+//         yield value;
+//       }
+//     },
+//   };
+//   for (const value of range) {
 //     rangeArray.push(value);
 //   }
 //   return rangeArray;
