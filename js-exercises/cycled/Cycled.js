406cf83fb1deca8f51fe2b13d764312ac927f555
anu3dev <Email>
2021-03-31 16:10:21 +0530

    cycled question solution add

--- js-exercises/cycled/Cycled.js
+++ js-exercises/cycled/Cycled.js
@@ -1,1 +1,96 @@
-// export { Cycled };
+class Cycled {
+  constructor(generatorArray) {
+    this.mainArray = generatorArray;
+    this.position = 0;
+  }
+
+  * [Symbol.iterator]() {
+    for (let i = 0; i < this.mainArray.length; i += 1) {
+      yield this.mainArray[i];
+    }
+  }
+
+  [Symbol.iterator]() {
+    const subArrary = [];
+    let start = this.position;
+    let count = 0;
+    while (count < this.mainArray.length) {
+      subArrary.push(this.mainArray[start]);
+      if (start === this.mainArray.length - 1) {
+        start = 0;
+      } else {
+        start += 1;
+      }
+      count += 1;
+    }
+    return subArrary.values();
+  }
+
+  next() {
+    if (this.position === this.mainArray.length - 1) {
+      this.position = 0;
+    } else {
+      this.position += 1;
+    }
+
+    function* nextGenerator(obj) {
+      yield obj.mainArray[obj.position];
+    }
+
+    return nextGenerator(this).next().value;
+  }
+
+  previous() {
+    if (this.position === 0) {
+      this.position = this.mainArray.length - 1;
+    } else {
+      this.position -= 1;
+    }
+
+    function* nextGenerator(obj) {
+      yield obj.mainArray[obj.position];
+    }
+
+    return nextGenerator(this).next().value;
+  }
+
+  current() {
+    function* currentGenerator(obj) {
+      yield obj.mainArray[obj.position];
+    }
+
+    return currentGenerator(this).next().value;
+  }
+
+  step(stepBy) {
+    function* stepGenerator(obj) {
+      yield obj.mainArray[obj.position];
+    }
+
+    if (this.position + stepBy >= this.mainArray.length) {
+      this.position -= this.mainArray.length - stepBy;
+    } else {
+      this.position += stepBy;
+    }
+    return stepGenerator(this).next().value;
+  }
+
+  get index() {
+    return this.position;
+  }
+
+  set index(val) {
+    if (val >= 0 && val <= this.mainArray.length) {
+      this.position = val;
+    }
+  }
+
+  indexOf(value) {
+    return this.mainArray.indexOf(value);
+  }
+
+  * reversed() {
+    yield this.mainArray[this.mainArray.length - 1 - this.position];
+  }
+}
+export { Cycled };
--- js-exercises/range-iter/rangeIter.js
+++ js-exercises/range-iter/rangeIter.js
@@ -1,42 +1,55 @@
 // implemented with function* and yield i.e. generator
 
-function rangeIter(lb, ub) {
-  if (ub === undefined || typeof ub !== 'number' || lb === undefined || typeof lb !== 'number') {
-    throw new TypeError('undefined is not a number');
-  }
-  const rangeArray = [];
-  function* generateSequence(start, end) {
-    for (let i = start; i <= end; i += 1) {
-      yield i;
-    }
-  }
-  for (const value of generateSequence(lb, ub)) {
-    rangeArray.push(value);
-  }
-  return rangeArray;
-}
-
-// implemented with * [ Symbol.iterator]() i.e. iterable
-
 // function rangeIter(lb, ub) {
 //   if (ub === undefined || typeof ub !== 'number' || lb === undefined || typeof lb !== 'number') {
 //     throw new TypeError('undefined is not a number');
 //   }
 //   const rangeArray = [];
-//   const range = {
-//     from: lb,
-//     to: ub,
-//     * [Symbol.iterator]() {
-//       for (let value = this.from; value <= this.to; value += 1) {
-//         yield value;
-//       }
-//     },
-//   };
-//   for (const value of range) {
+//   function* generateSequence(start, end) {
+//     for (let i = start; i <= end; i += 1) {
+//       yield i;
+//     }
+//   }
+//   for (const value of generateSequence(lb, ub)) {
 //     rangeArray.push(value);
 //   }
 //   return rangeArray;
 // }
+
+// implemented with iterator
+
+function rangeIter(lb, ub) {
+  if (ub === undefined || typeof ub !== 'number' || lb === undefined || typeof lb !== 'number') {
+    throw new TypeError('undefined is not a number');
+  }
+  const rangeArray = [];
+  let start = lb;
+  const end = ub;
+  const generateSequence = {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          if (start <= end) {
+            start += 1;
+            return {
+              value: start - 1,
+              done: false,
+            };
+          }
+          return {
+            done: true,
+          };
+        },
+      };
+    },
+  };
+
+  for (const value of generateSequence) {
+    rangeArray.push(value);
+  }
+
+  return rangeArray;
+}
 
 // implemented with normal js
 
